62d127b36db9bf40d2b1a5f99f02885e80fab5b5
Badal Kotecha <badal.kotecha@mindtree.com>
2018-09-17 15:51:40 -0400

    added customer service and few kubernetes configs

---
+++ Customer/Customer-Service/json/customer.js
@@ -0,0 +1,15 @@
+'use strict';
+ 
+class Customer {
+    constructor(id, firstName, lastName, emailAddress, dateOfBirth, address) {
+        this.id = id;
+        this.firstName = name;
+        this.lastName = price;
+        this.emailAddress = emailAddress;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address
+
+    }
+}
+ 
+module.exports = Customer;
---
+++ Customer/Customer-Service/repo/customerRepository.js
@@ -0,0 +1,42 @@
+'use strict';
+ 
+const Customer = require('../json/customer');
+ 
+class CustomerRepository {
+    constructor() {
+        this.customers = new Map([
+            [1, new Customer(1, 'Customer','1', 'customer1@domain.com', '01-01-1970', null )],
+            [2, new Customer(2, 'Customer','2', 'customer2@domain.com', '02-01-1970', null)],
+            [3, new Customer(3, 'Customer','3', 'customer3@domain.com', '03-01-1970', null)],
+            [4, new Customer(4, 'Customer','4', 'customer4@domain.com', '03-01-1970', null)]
+        ]);
+    }
+ 
+    getById(id) {
+        return this.Customers.get(id);
+    }
+ 
+    getAll() {
+        return Array.from(this.Customers.values());
+    }
+ 
+    remove() {
+        const keys = Array.from(this.Customers.keys());
+        this.Customers.delete(keys[keys.length - 1]);
+    }
+ 
+    save(Customer) {
+        if (this.getById(Customer.id) !== undefined) {
+            this.Customers[Customer.id] = Customer;
+            return "Updated Customer with id=" + Customer.id;
+        }
+        else {
+            this.Customers.set(Customer.id, Customer);
+            return "Added Customer with id=" + Customer.id;
+        }
+    }
+}
+ 
+const CustomerRepository = new CustomerRepository();
+ 
+module.exports = CustomerRepository;
