a5e25e8a107b5b878c9ed25c1bf822ffaa078535
Badal Kotecha <badal.kotecha@mindtree.com>
2018-09-19 23:01:30 -0400

    changed customer entity

--- Customer/Customer-Service/json/customer.js
+++ Customer/Customer-Service/json/customer.js
@@ -3,8 +3,8 @@
 class Customer {
     constructor(id, firstName, lastName, emailAddress, dateOfBirth, address) {
         this.id = id;
-        this.firstName = name;
-        this.lastName = price;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.emailAddress = emailAddress;
         this.dateOfBirth = dateOfBirth;
         this.address = address
