b22e9525b994f2796b08dc3bf23be7cfa018b71b
Badal Kotecha <badal.kotecha@mindtree.com>
2018-09-29 23:04:38 -0400

    changed customer order object ids

--- Customer/Customer-Order-Service/repo/customerOrderRepository.js
+++ Customer/Customer-Order-Service/repo/customerOrderRepository.js
@@ -5,16 +5,16 @@ const Customer = require('../json/customerOrder');
 class CustomerOrderRepository {
     constructor() {
         this.customerOrders = new Map([
-            // [1, new CustomerOrder(orderId=1, customerId=1, productId=1, quantity=5, unitPrice=15, new Date(year=2018, month=9, date=1) )],
+            // [1, new CustomerOrder(orderId=1, customerId=1, productId=1, quantity=5, unitPrice=15)],
 
-            [1, new CustomerOrder(1, 1, 1, 5, 15, new Date(year=2018, month=9, date=1) )],
-            [1, new CustomerOrder(1, 1, 2, 10, 10, new Date(year=2018, month=9, date=1) )],
-            [1, new CustomerOrder(2, 2, 1, 5, 15, new Date(year=2018, month=7, date=10) )],
-            [1, new CustomerOrder(2, 2, 3, 5, 23, new Date(year=2018, month=7, date=10) )],
-            [1, new CustomerOrder(2, 2, 4, 5, 32, new Date(year=2018, month=7, date=10) )],
-            [1, new CustomerOrder(3, 2, 2, 5, 10, new Date(year=2018, month=5, date=13) )],
-            [1, new CustomerOrder(4, 3, 3, 5, 23, new Date(year=2018, month=9, date=28) )],
-            [1, new CustomerOrder(4, 3, 4, 5, 35, new Date(year=2018, month=9, date=28) )]
+            [1, new CustomerOrder(1, 1, 1, 5, 15)],
+            [2, new CustomerOrder(1, 1, 2, 10, 10)],
+            [3, new CustomerOrder(2, 2, 1, 5, 15)],
+            [4, new CustomerOrder(2, 2, 3, 5, 23)],
+            [5, new CustomerOrder(2, 2, 4, 5, 32)],
+            [6, new CustomerOrder(3, 2, 2, 5, 10)],
+            [7, new CustomerOrder(4, 3, 3, 5, 23)],
+            [8, new CustomerOrder(4, 3, 4, 5, 35)]
         ]);
     }
  
