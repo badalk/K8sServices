244d59a1784676e0a2aecdf11fcae97bd431b0b9
Badal Kotecha <badal.kotecha@mindtree.com>
2018-12-26 13:38:19 -0500

    added sql code to product repository

--- Product/Product-Service/repo/productRepository.js
+++ Product/Product-Service/repo/productRepository.js
@@ -1,15 +1,32 @@
 'use strict';
  
 const Product = require('../json/product');
- 
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+//const sql = require('mssql')
+
+
+
+// var Connection = require('tedious').Connection;  
+// var config = {  
+//     userName: 'badal',  
+//     password: 'Welcome1234#',  
+//     server: 'k8sdbserver.database.windows.net',  
+//     // If you are on Microsoft Azure, you need this:  
+//     options: {encrypt: true, database: 'k8sdb'}  
+// };  
+// var connection = new Connection(config);  
+
+// connection.on('connect', function(err) {  
+// // If no error, then good to proceed.  
+//     console.log("Connected");  
+// });  
+
+
+
 class ProductRepository {
     constructor() {
-        this.products = new Map([
-            [1, new Product(1, 'Product 1', 10 )],
-            [2, new Product(2, 'Product 2', 20)],
-            [3, new Product(3, 'Product 3', 30)],
-            [4, new Product(4, 'Product 4', 40)]
-        ]);
+        this.products = {}
     }
  
     getById(id) {
@@ -17,8 +34,56 @@ class ProductRepository {
     }
  
     getAll() {
-        return Array.from(this.products.values());
+        var Connection = require('tedious').Connection;  
+        var Request = require('tedious').Request; 
+        
+        var config = {  
+            userName: 'badal',  
+            password: 'Welcome1234#',  
+            server: 'k8sdbserver.database.windows.net',  
+            // If you are on Microsoft Azure, you need this:  
+            options: {encrypt: true, database: 'k8sdb'}  
+        }; 
+
+        var connection = new Connection(config); 
+         
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.  
+            console.log("Connected");  
+            executeStatement(products);  
+        });  
+    
+        function executeStatement() {  
+            request = new Request("SELECT c.CustomerID, c.CompanyName,COUNT(soh.SalesOrderID) AS OrderCount FROM SalesLT.Customer AS c LEFT OUTER JOIN SalesLT.SalesOrderHeader AS soh ON c.CustomerID = soh.CustomerID GROUP BY c.CustomerID, c.CompanyName ORDER BY OrderCount DESC;", function(err) {  
+            if (err) {  
+                console.log(err);}  
+            });  
+            var result = "";  
+            request.on('row', function(columns) {  
+                // columns.forEach(function(column) {  
+                //   if (column.value === null) {  
+                //     console.log('NULL');  
+                //   } else {  
+                //     result+= column.value + " ";  
+                //   }  
+                // });  
+                var prod = new Product();
+                prod.id = columns["ProductId"];
+                prod.name = columns["Name"];
+                prod.price = columns["Price"];
+
+                this.products.push(prod);
+             
+            });  
+    
+            request.on('done', function(rowCount, more) {  
+                console.log(rowCount + ' rows returned');  
+            });  
+            connection.execSql(request);  
+        }   
+
     }
+
  
     remove() {
         const keys = Array.from(this.products.keys());
@@ -26,6 +91,45 @@ class ProductRepository {
     }
  
     save(product) {
+
+        var Connection = require('tedious').Connection;  
+        var config = {  
+            userName: 'badal',  
+            password: 'Welcome1234#',  
+            server: 'k8sdbserver.database.windows.net',  
+            // If you are on Azure SQL Database, you need these next options.  
+            options: {encrypt: true, database: 'k8sdb'}  
+        };  
+        var connection = new Connection(config);  
+        connection.on('connect', function(err) {  
+            // If no error, then good to proceed.  
+            console.log("Connected");  
+            executeStatement1();  
+        });  
+    
+        var Request = require('tedious').Request  
+        var TYPES = require('tedious').TYPES;  
+    
+        function executeStatement1() {  
+            request = new Request("INSERT Product (Name, Price) OUTPUT INSERTED.ProductID VALUES (@Name, @Price);", function(err) {  
+                        if (err) {  
+                            console.log(err);}  
+                        });  
+            request.addParameter('Name', TYPES.NVarChar, product.name);  
+            request.addParameter('Price', TYPES.Decimal, product.price);  
+            request.on('row', function(columns) {  
+                columns.forEach(function(column) {  
+                  if (column.value === null) {  
+                    console.log('NULL');  
+                  } else {  
+                    console.log("Product id of inserted item is " + column.value);  
+                    product.id = column.value
+                  }  
+                });  
+            });       
+            connection.execSql(request);  
+        }
+
         if (this.getById(product.id) !== undefined) {
             this.products[product.id] = product;
             return "Updated Product with id=" + product.id;
